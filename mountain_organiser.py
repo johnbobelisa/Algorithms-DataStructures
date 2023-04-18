b67bc8c663372fdf901bfcdabfe9c6efb41fcbbf
johnbobelisa <johnelisaaa@gmail.com>
2023-04-18 20:41:32 +1000

    fixed some stuff
    used the algorithms given this time

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from mountain import Mountain
+from algorithms import mergesort
 
 class MountainOrganiser:
-
+    
     def __init__(self) -> None:
         self.mountains = []
 
@@ -15,44 +16,14 @@ class MountainOrganiser:
         
         for m in self.mountains:
             m:Mountain
-            mountain_lengths.append((m.name,m.length))
+            mountain_lengths.append((m.length, m.name))
             
-        sorted_lst = merge_sort(mountain_lengths)
+        sorted_lst = mergesort.mergesort(mountain_lengths)
         
         for rank, values in enumerate(sorted_lst):
-            if mountain.name == values[0]:
+            if mountain.name == values[1]:
                 return rank
              
     def add_mountains(self, mountains: list[Mountain]) -> None:
         self.mountains += mountains
 
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst
-
-    mid = len(lst) // 2
-    right = lst[mid:]
-    left = lst[:mid]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
-
-def merge(left, right):
-    outcome = []
-    i = 0
-    j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i][1] <= right[j][1]:
-            outcome.append(left[i])
-            i += 1
-        else:
-            outcome.append(right[j])
-            j += 1
-
-    outcome += left[i:]
-    outcome += right[j:]
-
-    return outcome
