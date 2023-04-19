c25aee385d6dd3860eeebc1704711875bc534e6b
Tavern4990 <126471042+Tavern4990@users.noreply.github.com>
2023-04-20 01:01:23 +1000

    Update infinite_hash_table.py 2
    Second update.
    
    Created _infinite_probe method for reusability.
    
    Implemented __getitem__, __setitem__ and get_location methods.
    
    Will implement __delitem__ method.
    
    Passes test 4.1 in test_infinite_hash_table.py

--- infinite_hash_table.py
+++ infinite_hash_table.py
@@ -34,56 +34,73 @@ class InfiniteHashTable(Generic[K, V]):
         if self.level < len(key):
             return ord(key[self.level]) % (self.TABLE_SIZE-1)
         return self.TABLE_SIZE-1
-
+    
+    def _infinite_probe(self, key: K, is_insert: bool) -> ArrayR:
+        """
+        Find the correct table for this key
+        Changes self.level to the level the correct table is located.
+        """
+        current_table = self.top_table
+        
+        while True:
+            position = self.hash(key)
+            
+            if current_table[position] is None:
+                if is_insert:
+                    return current_table
+                else:
+                    raise KeyError(key)
+            
+            # matching key
+            elif current_table[position][0] == key:
+                return current_table
+            
+            # conflict, (key, table) at position
+            elif isinstance(current_table[position][1], ArrayR):
+                self.level += 1  
+                current_table = current_table[position][1]      # move to next hash table
+                continue
+            
+            # conflict, another (key, value) at position
+            else:
+                if is_insert:
+                    # Create new table and reinsert conflicting (key, value)
+                    other_key, other_value = current_table[position]        # current_table[position] contains (key, value)
+                    next_table = ArrayR(self.TABLE_SIZE)        # create another hash table
+                    next_table_key = other_key[:self.level + 1] + '*'      # change key to k*, ke*, key*, based on the level  
+                    current_table[position] = (next_table_key, next_table)     # change value to table  
+                    self.level += 1
+                    current_table = next_table      # move to next hash table
+                    other_position = self.hash(other_key)
+                    current_table[other_position] = (other_key, other_value)
+                    continue
+                else:
+                    raise KeyError(key)
+        
     def __getitem__(self, key: K) -> V:
         """
         Get the value at a certain key
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
+        current_table = self._infinite_probe(key, False)
+        position = self.hash(key)
+        self.level = 0      # reset self.level
+        return current_table[position][1]
 
     def __setitem__(self, key: K, value: V) -> None:
         """
         Set an (key, value) pair in our hash table.
         """
+        current_table = self._infinite_probe(key, True)
         position = self.hash(key)
+        self.level = 0      # reset self.level
         
-        # empty slot, insert (key, value)
-        if self.current_table[position] is None:
-            self.current_table[position] = (key, value)
-            self.count += 1
-            self.level = 0
-            self.current_table = self.top_table
-            return
+        if current_table[position] is None:   
+            self.count += 1     
         
-        # matching key, update value
-        elif self.current_table[position][0] == key:
-            self.current_table[position] = (key, value)
-            self.level = 0
-            self.current_table = self.top_table
-            return
-        
-        # conflict (key, table) at position
-        elif isinstance(self.current_table[position][1], ArrayR):
-            self.level += 1   
-            self.current_table = self.current_table[position][1]
-            self.__setitem__(key, value)
-        
-        # conflict another (key, value) at position 
-        else:
-            other_key, other_value = self.current_table[position]    # current_table[position] contains (key, value)
-            next_table = ArrayR(self.TABLE_SIZE)    # create another hash table
-            next_table_name = other_key[:self.level + 1] + '*'
-            # change key to k*, ke*, key*, based on the level   
-            # change value to table
-            self.current_table[position] = (next_table_name, next_table)     
-            self.current_table = next_table      # move to next hash table
-            self.level += 1
-            other_position = self.hash(other_key)
-            self.current_table[other_position] = (other_key, other_value)   # reinsert other_key, other value
-            self.__setitem__(key, value)
-            
+        current_table[position] = (key, value)
+    
 
     def __delitem__(self, key: K) -> None:
         """
@@ -112,7 +129,7 @@ class InfiniteHashTable(Generic[K, V]):
                 next_index = int(input("Choose the next table by its index: "))
                 self.current_table = self.current_table[next_index][1]
             except (ValueError, IndexError):
-                return "Done"
+                return "Ended"
 
     def get_location(self, key):
         """
@@ -120,8 +137,16 @@ class InfiniteHashTable(Generic[K, V]):
 
         :raises KeyError: when the key doesn't exist.
         """
-        raise NotImplementedError()
-
+        locations = []
+        self._infinite_probe(key, False)
+        
+        for i in range(self.level + 1):
+            self.level = i
+            locations.append(self.hash(key))
+        
+        self.level = 0      # reset self.level
+        return locations
+    
     def __contains__(self, key: K) -> bool:
         """
         Checks to see if the given key is in the Hash Table
@@ -144,3 +169,4 @@ if __name__ == "__main__":
     ih["linger"] = 8
     print(ih)
     print(len(ih))
+    print(ih.get_location("linger"))
