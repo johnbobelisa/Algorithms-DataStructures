b04fc5d732d2e5ff1cd655f70174a5fc1a371b2c
Tavern4990 <126471042+Tavern4990@users.noreply.github.com>
2023-05-06 16:06:05 +1000

    Implement mountain_organiser.py
    Implemented mountain_organiser.py
    
    Passes test 6.

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,58 +1,74 @@
 from __future__ import annotations
 
 from mountain import Mountain
-
+from algorithms.mergesort import mergesort
+from algorithms.binary_search import binary_search
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        self.mountains = []
+        self.organiser_list = []
 
     def cur_position(self, mountain: Mountain) -> int:
-        mountain_lengths = []
-
-        if mountain not in self.mountains:
-            raise KeyError()
-        
-        for m in self.mountains:
-            m:Mountain
-            mountain_lengths.append((m.name,m.length))
-            
-        sorted_lst = merge_sort(mountain_lengths)
-        
-        for rank, values in enumerate(sorted_lst):
-            if mountain.name == values[0]:
-                return rank
-             
-    def add_mountains(self, mountains: list[Mountain]) -> None:
-        self.mountains += mountains
-
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst
-
-    mid = len(lst) // 2
-    right = lst[mid:]
-    left = lst[:mid]
-
-    left = merge_sort(left)
-    right = merge_sort(right)
-
-    return merge(left, right)
-
-def merge(left, right):
-    outcome = []
-    i = 0
-    j = 0
-
-    while i < len(left) and j < len(right):
-        if left[i][1] <= right[j][1]:
-            outcome.append(left[i])
-            i += 1
+        """
+        Finds the rank of the provided mountain given all mountains included so far.
+        Raises KeyError if this mountain hasn't been added yet.
+        """ 
+        mountain_rank = binary_search(self.organiser_list, mountain)
+        if mountain_rank == -1:
+            raise KeyError(mountain)
         else:
-            outcome.append(right[j])
-            j += 1
+            return mountain_rank
+        
+    def add_mountains(self, mountains: list[Mountain]) -> None:
+        """
+        Adds a list of mountains to the organiser
+        """
+        new_mountains_sorted = mergesort(mountains)
+        
+        all_mountains_sorted = []   # new list to store all the mountains sorted
+        
+        index_a = 0     # index for self.organiser_list
+        index_b = 0     # index for new_mountains_sorted
+        
+        while index_a < len(self.organiser_list) and index_b < len(new_mountains_sorted):
+            if self.organiser_list[index_a] < new_mountains_sorted[index_b]:
+                all_mountains_sorted.append(self.organiser_list[index_a])
+                index_a += 1
+            else: 
+                all_mountains_sorted.append(new_mountains_sorted[index_b])
+                index_b += 1
+                
+        if index_a < len(self.organiser_list):  # mountains remaining in self.organiser_list
+            all_mountains_sorted += self.organiser_list[index_a:]
+        elif index_b < len(new_mountains_sorted):   # mountains remaining in new_mountains_sorted
+            all_mountains_sorted += new_mountains_sorted[index_b:]
+            
+        self.organiser_list = all_mountains_sorted
+            
 
-    outcome += left[i:]
-    outcome += right[j:]
+if __name__ == "__main__":
+    m1 = Mountain("m1", 2, 2)
+    m2 = Mountain("m2", 2, 9)
+    m3 = Mountain("m3", 3, 6)
+    m4 = Mountain("m4", 3, 1)
+    m5 = Mountain("m5", 4, 6)
+    m6 = Mountain("m6", 7, 3)
+    m7 = Mountain("m7", 7, 7)
+    m8 = Mountain("m8", 7, 8)
+    m9 = Mountain("m9", 7, 6)
+    m10 = Mountain("m10", 8, 4)
+    
+    mo = MountainOrganiser()
+    mo.add_mountains([m1, m2])
+    print(mo.organiser_list)
 
-    return outcome
+    mo.add_mountains([m4, m3])
+    print(mo.organiser_list)
+
+    mo.add_mountains([m5])
+    print(mo.organiser_list)
+    
+    mo.add_mountains([m7, m9, m6, m8])
+    print(mo.organiser_list)
+
+    mo.cur_position(m10)
